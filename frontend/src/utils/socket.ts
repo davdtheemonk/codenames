d3b4f5d726233b6d0f3facf23e549eb063a9f1e8
davdtheemonk <parishniclaus@gmail.com>
2025-02-03 06:14:54 +0300

    fix: update Dockerfile

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,3 +1,5 @@
 import socketIOClient from "socket.io-client";
 const WS = import.meta.env.VITE_BACKEND_URL;
-export const socket = socketIOClient(WS);
+export const socket = socketIOClient(
+  "http://ec2-13-53-49-118.eu-north-1.compute.amazonaws.com"
+);
