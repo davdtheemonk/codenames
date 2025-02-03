25126d627f287fdd71b13ce351354f83706183cf
davdtheemonk <parishniclaus@gmail.com>
2025-02-03 08:35:53 +0300

    fix: update README

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,5 +1,3 @@
 import socketIOClient from "socket.io-client";
 const WS = import.meta.env.VITE_BACKEND_URL;
-export const socket = socketIOClient(
-  "ws://ec2-13-53-49-118.eu-north-1.compute.amazonaws.com:3001"
-);
+export const socket = socketIOClient("13.53.49.118");
