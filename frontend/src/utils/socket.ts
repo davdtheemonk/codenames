618ff163aeed1ee25fb3528d2a869655e03c1162
davdtheemonk <parishniclaus@gmail.com>
2025-02-06 01:02:08 +0300

    env

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,3 +1,3 @@
 import socketIOClient from "socket.io-client";
 const WS = import.meta.env.VITE_BACKEND_URL;
-export const socket = socketIOClient(`wss://${WS}`);
+export const socket = socketIOClient(`ws://${WS}`);
